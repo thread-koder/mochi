580273512be00e531bac98994b53c5ed76a84876
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2026-01-27 15:34:23 +0300

    feat(web): rename UiSidebar to GlobalSidebar
    Signed-off-by: Mohamad Berjawi <mohamad.fberjawi@gmail.com>

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-screen overflow-hidden bg-surface text-on-surface min-h-screen font-body">
-    <UiSidebar />
+    <GlobalSidebar />
     <main class="flex-1 overflow-y-auto">
       <NuxtPage />
     </main>
