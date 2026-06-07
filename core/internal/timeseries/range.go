7ab4a6ab492275ae7690ded30a1240a4e4c0e5a4
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2026-06-08 01:58:46 +0300

    feat(timeseries): add tiered RangeStepForTimeRange for Prometheus queries
    Centralize range-query step selection so short windows keep 1m resolution
    for rightsizing while multi-day windows coarsen to bound query cost.
    
    Signed-off-by: Mohamad Berjawi <mohamad.fberjawi@gmail.com>

---
+++ core/internal/timeseries/range.go
@@ -0,0 +1,49 @@
+package timeseries
+
+import "time"
+
+const maxRangePoints = 11000
+
+// RangeStepForTimeRange returns an appropriate Prometheus range-query step for the given time range.
+// Shorter time ranges keep fine resolution for rightsizing, longer time ranges coarsen
+// to limit query cost.
+func RangeStepForTimeRange(timeRange time.Duration) time.Duration {
+	const day = 24 * time.Hour
+
+	var step time.Duration
+	switch {
+	case timeRange <= 2*day:
+		step = time.Minute
+	case timeRange <= 14*day:
+		step = 5 * time.Minute
+	case timeRange <= 45*day:
+		step = 15 * time.Minute
+	default:
+		step = 30 * time.Minute
+	}
+
+	return coarsenStepToPointCap(timeRange, step)
+}
+
+func coarsenStepToPointCap(timeRange, step time.Duration) time.Duration {
+	if step <= 0 {
+		step = time.Minute
+	}
+	for timeRange/step > maxRangePoints {
+		switch {
+		case step < 5*time.Minute:
+			step = 5 * time.Minute
+		case step < 15*time.Minute:
+			step = 15 * time.Minute
+		case step < 30*time.Minute:
+			step = 30 * time.Minute
+		case step < time.Hour:
+			step = time.Hour
+		case step < 4*time.Hour:
+			step = 4 * time.Hour
+		default:
+			step = 6 * time.Hour
+		}
+	}
+	return step
+}
